688d9fa677be67e400dd247d689e98cc3a58947a
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-18 16:31:38 +0000

    Maximum and minimum of an array using minimum number of comparisons

---
+++ GFG_problem/min_max.java
@@ -0,0 +1,58 @@
+// Question - https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
+
+// solution - 
+
+class Solution
+{ 
+    public static int findSum(int A[],int N) {
+    
+    //1st approach
+        
+    //     int min = Integer.MAX_VALUE;
+    //     int max = Integer.MIN_VALUE;
+        
+    //     for(int i=0; i< N; i++){
+    //         if(A[i]< min){
+    //             min = A[i];
+    //         }
+    //         if(A[i] > max){
+    //             max = A[i];
+    //         }
+    //     }
+    //     return max+min;
+    
+    // 2nd approach 
+    int min, max,i;
+    if(N%2 == 0){
+        if(A[0] > A[1]){
+           max = A[0];
+           min = A[1];
+        }
+        else{
+            max = A[1];
+            min = A[0];
+        }
+        i=2;
+    }
+    else{
+        min = max = A[0];
+        i=1;
+    }
+    for( ; i<N-1; i+=2){
+        if(A[i] > A[i+1]){
+            if(A[i] > max)
+                max = A[i];
+            if(A[i+1] < min)
+                min = A[i+1];
+        }
+        else{
+            if(A[i] < min)
+                min = A[i];
+            if(A[i+1] > max)
+                max = A[i+1];
+        }
+    }
+    return max+min;
+        
+    }
+}
