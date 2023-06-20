4a87d195752a17487b936dca5247046a10257f54
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-20 12:27:51 +0000

    Contains Duplicate

--- PW_DSA_Assignment/Assignment_01/Q6_contains_duplicate.java
+++ PW_DSA_Assignment/Assignment_01/Q6_contains_duplicate.java
@@ -1,3 +1,12 @@
+// Q6. Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
