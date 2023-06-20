7945f18678233b78c2df7fcd3c7acdea3cce6450
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-20 18:25:13 +0000

    Merge sorted aray

--- PW_DSA_Assignment/Assignment_01/Q5_merge_sorted_array.java
+++ PW_DSA_Assignment/Assignment_01/Q5_merge_sorted_array.java
@@ -16,11 +16,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        int i=m;
-        int j=0;
-        for(; i<m+n; i++, j++){
-            nums1[i]=nums2[j];
+        // 1st approach
+        // for(int i=m, j=0; i<m+n; i++, j++){
+        //     nums1[i]=nums2[j];
+        // }
+        // Arrays.sort(nums1);
+
+        //2nd approach
+        int p1=m-1;
+        int p2=n-1;
+        for(int i=m+n-1; i>=0; i--){
+            if(p2<0){
+                break;
+            }
+            if(p1 >= 0 && nums1[p1] > nums2[p2]){
+                nums1[i]=nums1[p1--];
+            }
+            else{
+                nums1[i]=nums2[p2--];
+            }
         }
-        Arrays.sort(nums1);
     }
 }
