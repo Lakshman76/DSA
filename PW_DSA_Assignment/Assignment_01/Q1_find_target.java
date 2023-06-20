12b71244436c1f2e1a3796d253928c0480ddae70
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-20 12:14:38 +0000

    find index of target

--- PW_DSA_Assignment/Assignment_01/Q1_find_target.java
+++ PW_DSA_Assignment/Assignment_01/Q1_find_target.java
@@ -1,3 +1,18 @@
+
+// 💡 **Q1.** Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// **Example:**
+// Input: nums = [2,7,11,15], target = 9
+// Output0 [0,1]
+
+// **Explanation:** Because nums[0] + nums[1] == 9, we return [0, 1]
+
+// ************************solution*************
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
