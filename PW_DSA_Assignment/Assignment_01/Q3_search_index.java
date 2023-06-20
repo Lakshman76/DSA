4977ec616013ab0a3c7715f44ddd44028db48ab8
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-20 12:19:00 +0000

    Searching index of target value

--- PW_DSA_Assignment/Assignment_01/Q3_search_index.java
+++ PW_DSA_Assignment/Assignment_01/Q3_search_index.java
@@ -1,3 +1,14 @@
+// **Q3.** Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// **Example 1:**
+// Input: nums = [1,3,5,6], target = 5
+
+// Output: 2
+
+// ******************Solution*******************
+
 class Solution {
     public int searchInsert(int[] nums, int target) {
         // first approach - O(n)
