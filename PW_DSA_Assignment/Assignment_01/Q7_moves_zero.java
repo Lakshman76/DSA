5be33adab03413c8fc414067e42975857c502442
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-20 09:23:54 +0000

    Moves zero at the end

---
+++ PW_DSA_Assignment/Assignment_01/Q7_moves_zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int k=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] !=0){
+                nums[k++] = nums[i];
+            }
+        }
+        while(k<nums.length){
+            nums[k++] = 0;
+        }
+    }
+}
