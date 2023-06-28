3c140dbddaa86863c0a8ea4aa0844607eadfd6ae
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-28 09:56:28 +0000

    find next permutation

---
+++ PW_DSA_Assignment/Assignment_03/Q3_next_permutation.java
@@ -0,0 +1,62 @@
+// 💡 Question 3
+// A permutation of an array of integers is an arrangement of its members into a
+// sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are all the permutations of arr:
+// [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+
+// The next permutation of an array of integers is the next lexicographically greater
+// permutation of its integer. More formally, if all the permutations of the array are
+// sorted in one container according to their lexicographical order, then the next
+// permutation of that array is the permutation that follows it in the sorted container.
+
+// If such an arrangement is not possible, the array must be rearranged as the
+// lowest possible order (i.e., sorted in ascending order).
+
+// ● For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// ● Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// ● While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not
+// have a lexicographical larger rearrangement.
+
+// Given an array of integers nums, find the next permutation of nums.
+// The replacement must be in place and use only constant extra memory.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+
+// solution - 
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index = -1;
+        int n = nums.length;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(nums, 0, n-1);
+        }
+        else{
+            for(int i=n-1; i>index; i--){
+            if(nums[i] > nums[index]){
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        reverse(nums, index+1, n-1);
+        }
+    }
+    public void reverse (int nums[], int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+}
