b8a8418be688bb2bc98ff4fb03d56bfb8d4970ce
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-06-28 17:01:21 +0000

    Transpose of a matrix

---
+++ PW_DSA_Assignment/Assignment_04/Q3_transpose_matrix.java
@@ -0,0 +1,28 @@
+//  Question 3
+// Given a 2D integer array matrix, return *the **transpose** of* matrix.
+
+// The **transpose** of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+// Example 1:
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+// Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+// Solution - 
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+       int row = matrix.length;
+       int col = matrix[0].length;
+
+       int [][] result = new int[col][row]; 
+
+       for(int i=0; i<matrix.length; i++){
+           for(int j=0; j<matrix[i].length; j++){
+               result[j][i] = matrix[i][j];
+           }
+       }
+       return result;
+    }
+}
