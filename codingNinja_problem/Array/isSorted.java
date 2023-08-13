f4a5c61186104e36f76eee75e836264b3e4c55b2
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-08-13 10:09:49 +0000

    check if the array is sorted or not

---
+++ codingNinja_problem/Array/isSorted.java
@@ -0,0 +1,17 @@
+// Question link - https://www.codingninjas.com/studio/problems/ninja-and-the-sorted-check_6581957?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
+
+// Solution - 
+
+public class Solution {
+    public static int isSorted(int n, int []a) {
+
+/****************** Optimal Approach ********************/
+
+        for (int i = 0; i < n-1; i++) {
+            if(a[i] > a[i+1]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
