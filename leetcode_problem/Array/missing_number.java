23022f53c0b6e08f1f717f1938711982727a0b9b
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-08-16 17:48:52 +0000

    find missing number

--- leetcode_problem/Array/missing_number.java
+++ leetcode_problem/Array/missing_number.java
@@ -4,15 +4,36 @@
 
 class Solution {
     public int missingNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i < nums.length; i++){
-            map.put(nums[i], i);
+
+        // 1st approach
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i=0; i < nums.length; i++){
+        //     map.put(nums[i], i);
+        // }
+        // for(int i=0; i <= nums.length; i++){
+        //     if(!map.containsKey(i)){
+        //         return i;
+        //     } 
+        // }
+        // return nums.length;
+
+        // Gauss's formula ---> 2nd approach
+
+        // int expectedSum = nums.length * (nums.length+1)/2;
+        // int actualSum = 0;
+
+        // for(int num : nums){
+        //     actualSum += num;
+        // }
+
+        // return (expectedSum - actualSum);
+
+        // using XOR 
+        int missingElement = nums.length;
+        
+        for(int i=0; i<nums.length; i++){
+            missingElement ^= i^nums[i];
         }
-        for(int i=0; i <= nums.length; i++){
-            if(!map.containsKey(i)){
-                return i;
-            } 
-        }
-        return nums.length;
+        return missingElement;
     }
 }
