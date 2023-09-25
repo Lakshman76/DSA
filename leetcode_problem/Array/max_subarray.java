e20f8b576c4b227b86b07821a87a43d10de34106
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2023-09-25 09:27:40 +0000

    optimized Maximum Subarray

--- leetcode_problem/Array/max_subarray.java
+++ leetcode_problem/Array/max_subarray.java
@@ -4,29 +4,43 @@
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        int neg_max = Integer.MIN_VALUE;
-        int flag = 0;
+        // int neg_max = Integer.MIN_VALUE;
+        // int flag = 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i] < 0){
+        //         flag++;
+        //     }
+        //     neg_max = Math.max(neg_max, nums[i]);
+        // }
+        // if(flag == nums.length){
+        //     return neg_max;
+        // }
+        // else{
+        //     int curr_sum = 0;
+        //     int maxm_sum = Integer.MIN_VALUE;
+        //     for(int i=0; i<nums.length; i++){
+        //         curr_sum += nums[i];
+        //         if(curr_sum < 0){
+        //             curr_sum = 0;
+        //         }
+        //         maxm_sum = Math.max(maxm_sum, curr_sum);
+        //     }
+        //     return maxm_sum;
+        // }
+
+        int sum = 0;
+        int maxm = nums[0];
         for(int i=0; i<nums.length; i++){
-            if(nums[i] < 0){
-                flag++;
+            sum += nums[i];
+            if(sum > maxm){
+                maxm = sum;
             }
-            neg_max = Math.max(neg_max, nums[i]);
-        }
-        if(flag == nums.length){
-            return neg_max;
-        }
-        else{
-            int curr_sum = 0;
-            int maxm_sum = Integer.MIN_VALUE;
-            for(int i=0; i<nums.length; i++){
-                curr_sum += nums[i];
-                if(curr_sum < 0){
-                    curr_sum = 0;
-                }
-                maxm_sum = Math.max(maxm_sum, curr_sum);
+            // kaddan's algorithm
+            if(sum < 0){
+                sum=0;
             }
-            return maxm_sum;
         }
+        return maxm;
         
     }
 }
