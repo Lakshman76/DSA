b31551ff322a59d4b4ab7085473606bdb0bb2755
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-09-21 17:42:24 +0000

    triangle pattern with 0 1

---
+++ self-learning/Pattern/Triangle_01.java
@@ -0,0 +1,29 @@
+/*
+e.g-
+1
+01
+101
+0101
+10101
+*/
+
+import java.util.Scanner;
+
+public class Triangle_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int row = sc.nextInt();
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
