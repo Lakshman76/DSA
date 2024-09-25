19a32396197b49f2b5a7ccaf49d4041970fc5e21
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-09-25 13:08:20 +0000

    print total sub array

---
+++ self-learning/Array/SubArray.java
@@ -0,0 +1,19 @@
+public class SubArray {
+
+    public static void printSubArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, -2, 6, -1, 3 };
+        printSubArray(arr);
+    }
+}
