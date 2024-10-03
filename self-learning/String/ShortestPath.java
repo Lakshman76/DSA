c6b5b9fd4a7ac155f9c6634517564d991970ba09
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-10-03 16:11:30 +0000

    find shortest path

---
+++ self-learning/String/ShortestPath.java
@@ -0,0 +1,27 @@
+public class ShortestPath {
+
+    public static float getShortestPath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            // EAST
+            if (dir == 'E') {
+                x++;
+            } else if (dir == 'W') {
+                x--;
+            } else if (dir == 'N') {
+                y++;
+            } else {
+                y--;
+            }
+        }
+        int x2 = x * x;
+        int y2 = y*y;
+        return (float)Math.sqrt(x2 + y2);
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+}
