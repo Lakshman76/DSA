9d984dcb1d1bd55931a42b25695348b9aa6afb11
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-10-05 15:52:59 +0000

    compression of string

---
+++ self-learning/String/StrCompression.java
@@ -0,0 +1,23 @@
+public class StrCompression {
+
+    public static String compress(String str){
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabbcccdddd";
+        System.out.println(compress(str));
+    }
+}
