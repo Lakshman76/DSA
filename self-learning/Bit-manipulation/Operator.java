4940ed326dcd14a1a66192a002251851d9b59db0
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-10-06 16:09:01 +0000

    add AND, OR, XOR operator

---
+++ self-learning/Bit-manipulation/Operator.java
@@ -0,0 +1,14 @@
+public class Operator {
+
+    public static void main(String[] args) {
+
+        // AND --> Both true than true
+        System.out.println(5 & 6); // 4
+
+        // OR --> Any one true than true
+        System.out.println(5 | 6); // 7
+
+        // XOR - Different - true(0 ^ 1 = 1), same - false(1 ^ 1 = 0)
+        System.out.println(5 ^ 6); // 3
+    }
+}
