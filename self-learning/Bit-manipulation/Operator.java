1f9485c48cdd6fd68f9aeba658a74e656d52525c
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-10-07 16:42:23 +0000

    add 1's complement, left shift, right shift

--- self-learning/Bit-manipulation/Operator.java
+++ self-learning/Bit-manipulation/Operator.java
@@ -10,5 +10,14 @@ public class Operator {
 
         // XOR - Different - true(0 ^ 1 = 1), same - false(1 ^ 1 = 0)
         System.out.println(5 ^ 6); // 3
+
+        // 1's complement
+        System.out.println(~11); // -6
+
+        // Binary LEFT Shift(<<) formula --> a<<b = a*(2^b)
+        System.out.println(5 << 2); // 20
+
+        // Biary RIGHT Shift(>>) formula --> a>>b = a/(2^b)
+        System.out.println(6 >> 1); // 3
     }
 }
