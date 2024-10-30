07c060e0b449fb0292246a72493ffb635371e039
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-10-30 16:08:20 +0000

    check number is power of two or not

---
+++ leetcode_problem/Bit-manipulation/IsPowerOfTwo.java
@@ -0,0 +1,13 @@
+// Question link - https://leetcode.com/problems/power-of-two/
+
+// Solution - 
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){
+            return false;
+        }
+
+        return (n & (n - 1)) == 0;
+    }
+}
