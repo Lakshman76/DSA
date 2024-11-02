ae9353003d5861294084b682cf42b9ce4c06fd16
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-11-02 12:46:17 +0000

    Minimum Bit Flips to Convert Number

---
+++ leetcode_problem/Bit-manipulation/MinBitsToFlip.java
@@ -0,0 +1,17 @@
+// Question link - https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+
+// solution - 
+
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int count = 0;
+        while(start > 0 || goal > 0){
+            if((start & 1) != (goal & 1)){
+                count++;
+            }
+            start = start >> 1;
+            goal = goal >> 1;
+        }
+        return count;
+    }
+}
