ff14e1c245f59d0ac67fb54096388263680bf0b4
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2024-11-02 13:29:46 +0000

    single number in an array

---
+++ leetcode_problem/Bit-manipulation/SingleNumber.java
@@ -0,0 +1,13 @@
+// Question link - https://leetcode.com/problems/single-number/
+
+// Solution - 
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
