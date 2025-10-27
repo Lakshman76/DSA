b0e3e0e85ebff73ba0068a1391210dacc2127115
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-10-27 06:12:06 +0000

    subset with duplicates

--- self-learning/Array/Subset.java
+++ self-learning/Array/Subset.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subset {
@@ -16,9 +17,28 @@ public class Subset {
         return outer;
     }
 
+    static List<List<Integer>> subSetWithDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+            int n = outer.size();
+            int start = 0;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                start = n / 2;
+            }
+            for (int j = start; j < n; j++) {
+                List<Integer> inner = new ArrayList<>(outer.get(j));
+                inner.add(nums[i]);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3 };
-        List<List<Integer>> ans = subSet(arr);
+        int[] arr = { 1, 2, 2, 3, 3 };
+        List<List<Integer>> ans = subSetWithDuplicates(arr);
         for (List<Integer> list : ans) {
             System.out.println(list);
         }
