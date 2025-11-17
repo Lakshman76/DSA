351c80a7740f0e5a70ce65c9ddfe51464263954a
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-11-17 02:42:26 +0000

    Letter Case Permutation

---
+++ self-learning/Recursion/LetterCasePermutation.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCasePermutation {
+    static List<String> letterCasePermutation(String p, String up, String s) {
+        if (p.length() == s.length()) {
+            List<String> li = new ArrayList<>();
+            li.add(p);
+            return li;
+        }
+        List<String> ans = new ArrayList<>();
+        char ch = up.charAt(0);
+        if (ch >= '0' && ch <= '9') {
+            ans.addAll(letterCasePermutation(p + ch, up.substring(1), s));
+        } else {
+            ans.addAll(letterCasePermutation(p + Character.toLowerCase(ch), up.substring(1), s));
+            ans.addAll(letterCasePermutation(p + Character.toUpperCase(ch), up.substring(1), s));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCasePermutation("", "A1b2", "A1b2"));
+    }
+}
