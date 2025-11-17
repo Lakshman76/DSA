340965ce5fd92aa971156e25e6b26b4f7803962b
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-11-17 02:42:47 +0000

    permutation of array

---
+++ self-learning/Recursion/PermutationOfArray.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PermutationOfArray {
+    static List<List<Integer>> permutation(List<Integer> p, int[] up, int[] nums) {
+        if (p.size() == nums.length) {
+            List<List<Integer>> li = new ArrayList<>();
+            li.add(new ArrayList<>(p));
+            return li;
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i <= p.size(); i++) {
+            // p.add(up[i]);
+            p.add(i, up[0]);
+            ans.addAll(permutation(p, Arrays.copyOfRange(up, 1, up.length), nums));
+            p.remove(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 1 };
+        List<List<Integer>> ans = permutation(new ArrayList<>(), nums, nums);
+        for (List<Integer> list : ans) {
+            System.out.println(list);
+        }
+
+    }
+}
