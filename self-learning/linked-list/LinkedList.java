e9a17b3c4062b99e80f24e999207174b8c27f927
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-11-25 15:55:54 +0000

    insert at end

---
+++ self-learning/linked-list/LinkedList.java
@@ -0,0 +1,44 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("NULL");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.insert(10);
+        ll.insert(20);
+        ll.insert(30);
+        ll.display();
+    }
+}
