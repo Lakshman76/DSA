a5a8421b9ae967ab91104adabf908bcb4c5a3925
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-11-27 16:48:10 +0000

    add node to starting in doubly linked list

---
+++ self-learning/linked-list/DoublyLL.java
@@ -0,0 +1,43 @@
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+public class DoublyLL {
+    Node head;
+
+    void insertFirst(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        head.prev = newNode;
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("NULL \n");
+    }
+
+    public static void main(String[] args) {
+        DoublyLL ll = new DoublyLL();
+        ll.insertFirst(10);
+        ll.insertFirst(20);
+        ll.insertFirst(30);
+        ll.display();
+    }
+}
