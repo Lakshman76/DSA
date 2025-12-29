bf01c178633652d77f401d320fb3482ef090faaf
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-12-29 11:07:06 +0000

    inbuilt stack and queue

---
+++ self-learning/stack-queue/InBuilt.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class InBuilt {
+
+    public static void main(String[] args) {
+
+        // In built stack
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(10);
+        stack.push(30);
+        System.out.println(stack.push(40));
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+
+        // In built queue
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(4);
+        queue.add(5);
+        queue.add(7);
+        queue.add(6);
+        System.out.println(queue);
+        System.out.println(queue.remove());
+        System.out.println(queue.peek());
+    }
+}
