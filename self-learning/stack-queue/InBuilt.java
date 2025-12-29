c7fdf6d52dc69217b7ec6a167180dc500692a518
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-12-29 11:15:45 +0000

    inbuilt deque

--- self-learning/stack-queue/InBuilt.java
+++ self-learning/stack-queue/InBuilt.java
@@ -1,3 +1,5 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -27,5 +29,17 @@ public class InBuilt {
         System.out.println(queue);
         System.out.println(queue.remove());
         System.out.println(queue.peek());
+
+        // inbuilt Deque
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(5);
+        deque.add(7);
+        deque.addFirst(6);
+        deque.addLast(2);
+        deque.add(14);
+        System.out.println(deque);
+        System.out.println(deque.remove());
+        System.out.println(deque.removeLast());
+        System.out.println(deque);
     }
 }
