b447a8bac5df7bf6e8c599a7db9c7eece4c68149
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2025-12-30 04:53:44 +0000

    dynamic stack implementation

---
+++ self-learning/stack-queue/DynamicStack.java
@@ -0,0 +1,23 @@
+public class DynamicStack extends CustomStack {
+    public DynamicStack() {
+        super();
+    }
+
+    public DynamicStack(int size) {
+        super(size);
+    }
+
+    @Override
+    public int push(int item) {
+        if (isFull()) {
+            int[] temp = new int[data.length * 2];
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+
+        return super.push(item);
+    }
+
+}
