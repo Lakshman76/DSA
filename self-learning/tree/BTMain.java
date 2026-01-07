6c9fc0ba0aa008afa50537103a3aa0e7d12fb063
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-01-07 06:42:38 +0000

    Implement binary tree

---
+++ self-learning/tree/BTMain.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BTMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(sc);
+        tree.prettyDisplay();
+    }
+}
---
+++ self-learning/tree/BinaryTree.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class BinaryTree {
+    private static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    // Insert Element
+    public void populate(Scanner sc) {
+        System.out.println("Enter the root node: ");
+        int value = sc.nextInt();
+        root = new Node(value);
+        populate(sc, root);
+    }
+
+    private void populate(Scanner sc, Node node) {
+        System.out.println("Do you want to insert left node of " + node.value);
+        boolean left = sc.nextBoolean();
+        if (left) {
+            System.out.println("Enter value of left node of " + node.value);
+            int value = sc.nextInt();
+            node.left = new Node(value);
+            populate(sc, node.left);
+        }
+
+        System.out.println("Do you want to insert right node of " + node.value);
+        boolean right = sc.nextBoolean();
+        if (right) {
+            System.out.println("Enter value of right node of " + node.value);
+            int value = sc.nextInt();
+            node.right = new Node(value);
+            populate(sc, node.right);
+        }
+    }
+
+    public void display() {
+        display(root, "");
+    }
+
+    private void display(Node node, String indent) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, indent + "\t");
+        display(node.right, indent + "\t");
+    }
+
+    public void prettyDisplay() {
+        prettyDisplay(root, 0);
+    }
+
+    private void prettyDisplay(Node node, int level) {
+        if (node == null) {
+            return;
+        }
+        prettyDisplay(node.right, level + 1);
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t");
+            }
+            System.out.println("|------>" + node.value);
+
+        } else {
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left, level + 1);
+    }
+}
