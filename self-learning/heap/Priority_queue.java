dd2a79554e220cb44860271a5a62588070de59e3
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-02-10 14:41:34 +0000

    priority queue for max heap

--- self-learning/heap/Priority_queue.java
+++ self-learning/heap/Priority_queue.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Priority_queue {
@@ -16,18 +17,43 @@ public class Priority_queue {
         pq.offer(20);
         pq.offer(2);
 
-        System.out.println(pq);
+        System.out.println(pq); // [2, 12, 10, 32, 15, 22, 20]
 
-        System.out.println("Top: " + pq.peek());
+        System.out.println("Top: " + pq.peek()); // 2
 
         pq.poll();
         pq.poll();
 
-        System.out.println(pq);
+        System.out.println(pq); // [12, 15, 20, 32, 22]
+    }
+
+    // Max Heap
+    static void maxHeap() {
+        System.out.println("*************************** MAX HEAP *********************************");
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.offer(2);
+        pq.add(10);
+        pq.offer(22);
+        pq.offer(12);
+        pq.offer(15);
+        pq.offer(20);
+        pq.offer(32);
+
+        System.out.println(pq); // [32, 15, 22, 2, 12, 10, 20]
+
+        System.out.println("Top: " + pq.peek()); // 32
+
+        pq.poll();
+        pq.poll();
+
+        System.out.println(pq); // [20, 15, 10, 2, 12]
     }
 
     public static void main(String[] args) {
         minHeap();
 
+        maxHeap();
     }
 }
