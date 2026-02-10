215069e5a3d4633eae02d16c5d17747c4f7c3524
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-02-10 17:14:40 +0000

    Minimum cost of rope

--- self-learning/heap/HeapInterview.java
+++ self-learning/heap/HeapInterview.java
@@ -1,4 +1,8 @@
+import java.util.PriorityQueue;
+
 public class HeapInterview {
+
+    // find height of heap
     static int heapHeight(int n, int arr[]) {
         // code here
         if (n == 1)
@@ -27,9 +31,34 @@ public class HeapInterview {
         return height;
     }
 
+    // Minimum cost of rope
+    public static int minCost(int[] arr) {
+        if (arr.length <= 1) {
+            return 0;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int num : arr) {
+            pq.offer(num);
+        }
+
+        int ans = 0;
+
+        while (pq.size() > 1) {
+            int sum = pq.poll();
+            sum += pq.poll();
+            pq.offer(sum);
+            ans += sum;
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 3, 6, 5, 9, 8 };
 
         System.out.println(heapHeight(arr.length, arr));
+
+        System.out.println(minCost(arr));
     }
 }
