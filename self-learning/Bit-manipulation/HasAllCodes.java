d2d3a635dd7526252576c3884fb8cdf6972c60aa
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-02-23 03:36:04 +0000

    optimized

--- self-learning/Bit-manipulation/HasAllCodes.java
+++ self-learning/Bit-manipulation/HasAllCodes.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class HasAllCodes {
 
@@ -26,9 +28,29 @@ public class HasAllCodes {
         return list;
     }
 
+    // Optimized way to solve
+
+    public static boolean hasAllCodesOptimized(String s, int k) {
+        int n = s.length();
+
+        // total possible combination is less than 2 ^ k
+        if (n - k + 1 < (1 << k)) {
+            return false;
+        }
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i <= n - k; i++) {
+            set.add(s.substring(i, i + k));
+        }
+
+        return set.size() == (1 << k);
+    }
+
     public static void main(String[] args) {
         String s = "00110110";
         int k = 2;
         System.out.println(hasAllCodes(s, k));
+
+        System.out.println(hasAllCodesOptimized(s, k));
     }
 }
