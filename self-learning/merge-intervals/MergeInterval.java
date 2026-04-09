89b654eafc95d5eaee1aa9a4a7e6dd9a2c63e2c8
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-04-09 17:51:37 +0000

    Optimized Merge Intervals

--- self-learning/merge-intervals/MergeInterval.java
+++ self-learning/merge-intervals/MergeInterval.java
@@ -33,6 +33,25 @@ public class MergeInterval {
         return ans;
     }
 
+    static int[][] mergeOptimised(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+        ArrayList<int[]> list = new ArrayList<>();
+        for (int i = 1; i < intervals.length; i++) {
+            if (end1 >= intervals[i][0]) {
+                end1 = Math.max(end1, intervals[i][1]);
+                continue;
+            }
+            list.add(new int[] { start1, end1 });
+            start1 = intervals[i][0];
+            end1 = intervals[i][1];
+        }
+        list.add(new int[] { start1, end1 });
+
+        return list.toArray(new int[list.size()][]);
+    }
+
     public static void main(String[] args) {
         int[][] intervals = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
         int[][] ans = merge(intervals);
