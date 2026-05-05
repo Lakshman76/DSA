5c014ec4978b4990e90e4e797b34409dbb392be2
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-05-05 17:50:10 +0000

    Sum of xor of all pairs optimised

--- self-learning/potd/SumOfXor.java
+++ self-learning/potd/SumOfXor.java
@@ -11,8 +11,26 @@ public class SumOfXor {
         return ans;
     }
 
+    static long sumXOROptimized(int[] arr) {
+        long ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int countOne = 0;
+            int countZero = 0;
+            int bitMask = 1 << i;
+            for (int j = 0; j < arr.length; j++) {
+                if ((arr[j] & bitMask) == 0) {
+                    countZero++;
+                } else {
+                    countOne++;
+                }
+            }
+            ans += (long) countZero * countOne * (1 << i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 7, 3, 5 };
-        System.out.println(sumXOR(arr));
+        System.out.println(sumXOROptimized(arr));
     }
 }
