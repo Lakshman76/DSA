5ed768c11f89c4192234cceda4ee1f4563685446
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-05-31 04:17:26 +0000

    Express as Consecutive Number Sum

---
+++ self-learning/potd/ConsecutiveNumberSum.java
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/consecutive-numbers-for-sum3132/1
+public class ConsecutiveNumberSum {
+    static boolean isSumOfConsecutive(int n) {
+        if (n <= 2) {
+            return false;
+        }
+        int left = 1;
+        int sum = 0;
+        for (int i = 1; i <= n / 2 + 1; i++) {
+            sum += i;
+            while (sum > n) {
+                sum -= left;
+                left++;
+            }
+            if (sum == n)
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSumOfConsecutive(7));
+    }
+}
