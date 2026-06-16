52ebcb8922556cf709a8e589440a4995e59c18ec
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-06-16 06:08:39 +0000

    Optimized Construct List using XOR Queries

--- self-learning/potd/ConstructListXOR.java
+++ self-learning/potd/ConstructListXOR.java
@@ -26,8 +26,28 @@ public class ConstructListXOR {
         return list;
     }
 
+    static ArrayList<Integer> constructListOptimized(int[][] queries) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        int xor = 0;
+        for (int i = 0; i < queries.length; i++) {
+            int query = queries[i][0];
+            int x = queries[i][1];
+            if (query == 1) {
+                xor ^= x;
+            } else {
+                list.add(xor ^ x);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, (list.get(i) ^ xor));
+        }
+        list.sort(Comparator.naturalOrder());
+        return list;
+    }
+
     public static void main(String[] args) {
         int[][] queries = { { 0, 6 }, { 0, 3 }, { 0, 2 }, { 1, 4 }, { 1, 5 } };
-        System.out.println(constructList(queries));
+        System.out.println(constructListOptimized(queries));
     }
 }
