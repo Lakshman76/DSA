92c85a8c4abb8f759da90cfa4a00e22abc35f756
Lakshman kumar <107758274+Lakshman76@users.noreply.github.com>
2026-06-16 06:07:50 +0000

    Construct List using XOR Queries

---
+++ self-learning/potd/ConstructListXOR.java
@@ -0,0 +1,33 @@
+// https://www.geeksforgeeks.org/problems/construct-list-using-given-q-xor-queries/1
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class ConstructListXOR {
+    static void findXOR(ArrayList<Integer> list, int x) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, (list.get(i) ^ x));
+        }
+    }
+
+    static ArrayList<Integer> constructList(int[][] queries) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        for (int i = 0; i < queries.length; i++) {
+            int query = queries[i][0];
+            int x = queries[i][1];
+            if (query == 1) {
+                findXOR(list, x);
+            } else {
+                list.add(x);
+            }
+        }
+        list.sort(Comparator.naturalOrder());
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[][] queries = { { 0, 6 }, { 0, 3 }, { 0, 2 }, { 1, 4 }, { 1, 5 } };
+        System.out.println(constructList(queries));
+    }
+}
